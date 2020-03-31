331384f3f66403997625eed2911d5bd08c9f7c84
qw4wer <qw5wer@gmail.com>
2020-04-01 00:21:19 +0800

    modify

--- utils/youtube.js
+++ utils/youtube.js
@@ -8,11 +8,16 @@ const finished = util.promisify(stream.finished);
 // TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
 // TypeScript: import ytdl = require('ytdl-core'); with neither of the above
 
+const jsonpath = require('jsonpath');
+
+let m3u8stream = require('m3u8stream');
+
+
 const SocksProxyAgent = require('socks-proxy-agent');
 
 const info = {
     host: '192.168.0.1',
-    port:1080
+    port: 1080
 };
 const agent = new SocksProxyAgent(info);
 
@@ -35,8 +40,31 @@ let i = 0;
 //   .pipe(fs.createWriteStream('video.mp3'));
 
 //
-ytdl.getInfo('https://www.youtube.com/watch?v=9-FxgGmYMGU', {
+ytdl.getInfo('https://www.youtube.com/watch?v=9aGg62rdMtc', {
     requestOptions: {agent}
-}, (err, info) => {
-    console.log(JSON.stringify(info))
+}).then(info => {
+
+    const json = JSON.parse(JSON.stringify(info));
+
+    const m3u8Url = jsonpath.value(json, "$..formats[?(@.qualityLabel=='144p' && @.container=='ts')].url");
+
+    return Promise.resolve(m3u8Url);
+}).then(url => {
+    const u8stream = m3u8stream(url, {liveBuffer:1000,requestOptions: {agent}});
+    // u8stream.on('data', (chunk) => {
+    //     const bufferStream = new stream.PassThrough();
+    //     bufferStream.end(chunk);
+    //     bufferStream.pipe(fs.createWriteStream(`D:\\tmp\\${i}.ts`))
+    //     finished(bufferStream).then(() => {
+    //         i++
+    //     })
+    // });
+
+    u8stream.pipe(fs.createWriteStream('D:\\tmp\\videofile.mp4'));
+
+    setTimeout(()=>{
+        u8stream.end()
+        console.log(100)
+    },5000)
+
 })
